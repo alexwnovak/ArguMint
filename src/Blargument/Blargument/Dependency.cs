ab9ee235c09c1e0891935e635e0c2f8b503e0ebd
Alex Novak <alexwnovak@gmail.com>
2015-03-02 20:46:46 -0500

    +AutoInitialize flag to skip for-reals type mappings
    This is set to true by default, which will allow for real mapping from interfaces to concrete implementations.
    
    Unit tests can set this flag to false to obtain an empty container, and insert its own mappings for mocks.

--- src/Blargument/Blargument/Dependency.cs
+++ src/Blargument/Blargument/Dependency.cs
@@ -5,6 +5,19 @@ namespace Blargument
 {
    internal static class Dependency
    {
+      private static bool _autoInitialize = true;
+      public static bool AutoInitialize
+      {
+         get
+         {
+            return _autoInitialize;
+         }
+         set
+         {
+            _autoInitialize = value;
+         }
+      }
+
       private static readonly Lazy<UnityContainer> _unityContainer = new Lazy<UnityContainer>( CreateUnityContainer, true );
       public static UnityContainer UnityContainer
       {
@@ -17,6 +30,11 @@ namespace Blargument
       internal static UnityContainer CreateUnityContainer()
       {
          var unityContainer = new UnityContainer();
+
+         if ( AutoInitialize )
+         {
+            // Real wire-ups go here
+         }
 
          return unityContainer;
       }
