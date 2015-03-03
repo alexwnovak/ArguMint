80c2717ee01db7270f415a59b33da6f56bd56eda
Alex Novak <alexwnovak@gmail.com>
2015-03-02 19:37:04 -0500

    +MissingAttributesException class

---
+++ src/Blargument/Blargument/MissingAttributesException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class MissingAttributesException : Exception
+{
+   public MissingAttributesException()
+   {
+   }
+
+   public MissingAttributesException( string message )
+      : base( message )
+   {
+   }
+
+   public MissingAttributesException( string message, Exception inner )
+      : base( message, inner )
+   {
+   }
+
+   protected MissingAttributesException( SerializationInfo info, StreamingContext context )
+      : base( info, context )
+   {
+   }
+}
