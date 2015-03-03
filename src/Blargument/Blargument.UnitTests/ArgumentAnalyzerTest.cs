ca9cc60c8feed538ca943b644cc423fcabab0666
Alex Novak <alexwnovak@gmail.com>
2015-03-02 19:33:04 -0500

    Analyzer now throws if argument array is null

--- src/Blargument/Blargument.UnitTests/ArgumentAnalyzerTest.cs
+++ src/Blargument/Blargument.UnitTests/ArgumentAnalyzerTest.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Blargument.UnitTests
 {
    [TestClass]
    public class ArgumentAnalyzerTest
    {
+      [TestMethod]
+      [ExpectedException( typeof( ArgumentException ) )]
+      public void Analyze_ArgumentsAreNull_ThrowsArgumentException()
+      {
+         ArgumentAnalyzer.Analyze<Any>( null );
+      }
    }
 }
--- src/Blargument/Blargument/ArgumentAnalyzer.cs
+++ src/Blargument/Blargument/ArgumentAnalyzer.cs
@@ -1,6 +1,17 @@
-﻿namespace Blargument
+﻿using System;
+
+namespace Blargument
 {
-   public class ArgumentAnalyzer
+   public static class ArgumentAnalyzer
    {
+      public static T Analyze<T>( string[] arguments ) where T : class, new()
+      {
+         if ( arguments == null )
+         {
+            throw new ArgumentException( "Arguments must not be null", "arguments" );
+         }
+
+         throw new NotImplementedException();
+      }
    }
 }
