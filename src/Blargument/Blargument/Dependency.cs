cce23cac41ae203d381920690568c68cb7f8f6f1
Alex Novak <alexwnovak@gmail.com>
2015-03-02 20:45:54 -0500

    +UnityContainer lazy property
    This allows us to always have an instantiated UnityContainer without first proactively creating it.

--- src/Blargument/Blargument/Dependency.cs
+++ src/Blargument/Blargument/Dependency.cs
@@ -1,6 +1,24 @@
-﻿namespace Blargument
+﻿using System;
+using Microsoft.Practices.Unity;
+
+namespace Blargument
 {
    internal static class Dependency
    {
+      private static readonly Lazy<UnityContainer> _unityContainer = new Lazy<UnityContainer>( CreateUnityContainer, true );
+      public static UnityContainer UnityContainer
+      {
+         get
+         {
+            return _unityContainer.Value;
+         }
+      }
+
+      internal static UnityContainer CreateUnityContainer()
+      {
+         var unityContainer = new UnityContainer();
+
+         return unityContainer;
+      }
    }
 }
