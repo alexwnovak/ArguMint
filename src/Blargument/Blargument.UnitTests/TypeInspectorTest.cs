3ebe3b3adebb6bd8f75512aa7898948819afd8e4
Alex Novak <alexwnovak@gmail.com>
2015-03-05 16:09:04 -0500

    +Test to return no marked properties if the type doesn't have any

--- src/Blargument/Blargument.UnitTests/TypeInspectorTest.cs
+++ src/Blargument/Blargument.UnitTests/TypeInspectorTest.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Blargument.UnitTests.Dummies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Blargument.UnitTests
 {
    [TestClass]
    public class TypeInspectorTest
    {
+      [TestMethod]
+      public void GetMarkedProperties_TypeHasNoProperties_ReturnsEmptyArrayOfMarkedProperties()
+      {
+         var typeInspector = new TypeInspector();
+
+         var markedProperties = typeInspector.GetMarkedProperties<ClassWithNoAttributes, Attribute>();
+
+         Assert.AreEqual( 0, markedProperties.Length );
+      }
    }
 }
