e55c89c07f2515ec1b4a94afbc1530fea2b3d11c
Alex Novak <alexwnovak@gmail.com>
2015-03-05 16:06:12 -0500

    +TypeInspector concrete implementation
    This bundles up public instance properties with the instance of its typed attribute.

---
+++ src/Blargument/Blargument/TypeInspector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Blargument
+{
+   internal class TypeInspector : ITypeInspector
+   {
+      public MarkedProperty<TAttribute>[] GetMarkedProperties<TType, TAttribute>() where TAttribute : Attribute
+      {
+         return (from p in typeof( TType ).GetProperties( BindingFlags.Public | BindingFlags.Instance )
+                 let attributes = p.GetCustomAttributes( typeof( TAttribute ), false ).Cast<TAttribute>()
+                 where attributes.Any()
+                 select new MarkedProperty<TAttribute>( p, attributes.First() )).ToArray();
+      }
+   }
+}
