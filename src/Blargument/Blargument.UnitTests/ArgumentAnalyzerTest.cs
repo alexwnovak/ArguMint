9700968bd0a22c1a8951f980139e5251593e5fa4
Alex Novak <alexwnovak@gmail.com>
2015-03-05 16:48:12 -0500

    +Test to throw if TypeInspector reports no ArgumentAttribute instances

--- src/Blargument/Blargument.UnitTests/ArgumentAnalyzerTest.cs
+++ src/Blargument/Blargument.UnitTests/ArgumentAnalyzerTest.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using Microsoft.Practices.Unity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Blargument.UnitTests.Dummies;
 
 namespace Blargument.UnitTests
 {
    [TestClass]
    public class ArgumentAnalyzerTest
    {
+      [TestInitialize]
+      public void Initialize()
+      {
+         Dependency.AutoInitialize = false;
+      }
+
       [TestMethod]
       [ExpectedException( typeof( ArgumentException ) )]
       public void Analyze_ArgumentsAreNull_ThrowsArgumentException()
       {
          ArgumentAnalyzer.Analyze<Any>( null );
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( MissingAttributesException ) )]
+      public void Analyze_TypeNotDecoratedWithAnyAttributes_ThrowsMissingAttributesException()
+      {
+         var markedProperties = new MarkedProperty<ArgumentAttribute>[0];
+
+         // Setup
+
+         var typeInspectorMock = new Mock<ITypeInspector>();
+         typeInspectorMock.Setup( ti => ti.GetMarkedProperties<ClassWithNoAttributes, ArgumentAttribute>() ).Returns( markedProperties );
+         Dependency.UnityContainer.RegisterInstance( typeInspectorMock.Object );
+
+         // Test
+
+         ArgumentAnalyzer.Analyze<ClassWithNoAttributes>( new string[0] );
       }
    }
 }
--- src/Blargument/Blargument/ArgumentAnalyzer.cs
+++ src/Blargument/Blargument/ArgumentAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Practices.Unity;
 
 namespace Blargument
 {
@@ -9,6 +10,15 @@ namespace Blargument
          if ( arguments == null )
          {
             throw new ArgumentException( "Arguments must not be null", "arguments" );
+         }
+
+         var typeInspector = Dependency.UnityContainer.Resolve<ITypeInspector>();
+
+         var markedProperties = typeInspector.GetMarkedProperties<T, ArgumentAttribute>();
+
+         if ( markedProperties.Length == 0 )
+         {
+            throw new MissingAttributesException( "No Argument attributes were found on class: " + typeof( T ).Name );
          }
 
          throw new NotImplementedException();
