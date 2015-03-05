2fb39bf9f498b4b28d7546b9ef78b61f60629da0
Alex Novak <alexwnovak@gmail.com>
2015-03-05 16:03:01 -0500

    +MarkedProperty class to bundle a property and its attached attribute

---
+++ src/Blargument/Blargument/MarkedProperty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace Blargument
+{
+   internal class MarkedProperty<T> where T: Attribute
+   {
+      private readonly PropertyInfo _propertyInfo;
+      public PropertyInfo PropertyInfo
+      {
+         get
+         {
+            return _propertyInfo;
+         }
+      }
+
+      private readonly T _attribute;
+      public T Attribute
+      {
+         get
+         {
+            return _attribute;
+         }
+      }
+
+      public MarkedProperty( PropertyInfo propertyInfo, T attribute )
+      {
+         _propertyInfo = propertyInfo;
+         _attribute = attribute;
+      }
+   }
+}
