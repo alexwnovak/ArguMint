3269f8962c174620737f9306d7e6d64c16764f9f
Alex Novak <alexwnovak@gmail.com>
2015-03-23 20:36:22 -0400

    +DontCareAttribute to allow use as a type parameter

---
+++ src/ArguMint/ArguMint.UnitTests/DontCareAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ArguMint.UnitTests
+{
+   internal class DontCareAttribute : Attribute
+   {
+      // Empty implementation to allow this as a type parameter that needs to be an attribute
+   }
+}
