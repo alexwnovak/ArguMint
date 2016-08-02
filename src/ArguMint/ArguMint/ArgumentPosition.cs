415c3f0e527d4012346b72e67a62b1f44e20b7cd
Alex Novak <alexwnovak@gmail.com>
2016-08-02 19:00:24 -0400

    +ArgumentPosition enum
    This describes where the argument is required to live. The default
    enum value (0) is "Any," where it doesn't try to match positions.

---
+++ src/ArguMint/ArguMint/ArgumentPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace ArguMint
+{
+   public enum ArgumentPosition
+   {
+      Any,
+      First,
+      Second,
+      Third,
+      Fourth,
+      Fifth,
+      Sixth,
+      Seventh,
+      Eighth,
+      Ninth,
+      Tenth
+   }
+}
