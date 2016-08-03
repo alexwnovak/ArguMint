1fb1466929a13e0e9732267d883329f33c05670f
Alex Novak <alexwnovak@gmail.com>
2016-08-02 20:46:17 -0400

    +Extension method to convert an ArgumentPosition enum to an index
    We'll use this to turn a symbolic enum value into an index we can use
    to get into an array. We'll use this to safely convert, instead of
    sprinkling logic to cast and possibly subtract. This keeps us safe in
    the event the enum values change to something less predictable.

---
+++ src/ArguMint/ArguMint/ArgumentPositionExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace ArguMint
+{
+   public static class ArgumentPositionExtensions
+   {
+      public static int ToIndex( this ArgumentPosition argumentPosition )
+         => (int) argumentPosition - (int) ArgumentPosition.First;
+   }
+}
