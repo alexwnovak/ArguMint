8b3068f7413a046b6a32f7bbd41803fb38e1970e
Alex Novak <alexwnovak@gmail.com>
2016-08-05 10:10:14 -0400

    Can now add a property to the dynamic class
    This does the IL munging for the getter and setter methods--nothing
    special, just returns and sets the field.
    
    This also caches the PropertyBuilder by the property name. We'll use
    this so other methods can add onto this property later (like for
    adding attribute instances).

--- src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilder.cs
+++ src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilder.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using ArguMint.UnitTests.Helpers;
 
 namespace ArguMint.UnitTests.Dynamic
 {
    public class ClassBuilder
    {
       private readonly TypeBuilder _typeBuilder;
+      private readonly Dictionary<string, PropertyBuilder> _propertyBuilders = new Dictionary<string, PropertyBuilder>();
 
       private ClassBuilder( TypeBuilder typeBuilder )
       {
@@ -32,6 +35,41 @@ namespace ArguMint.UnitTests.Dynamic
             typeof( object ) );
 
          return new ClassBuilder( typeBuilder );
+      }
+
+      public void AddProperty<T>( string name )
+      {
+         var fieldBuilder = _typeBuilder.DefineField( $"_{name}", typeof( T ), FieldAttributes.Private );
+         var propertyBuilder = _typeBuilder.DefineProperty( name, PropertyAttributes.HasDefault, typeof( T ), null );
+
+         _propertyBuilders[name] = propertyBuilder;
+
+         var getterSetterAttributes = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+
+         // Build the property "getter"
+
+         var getterBuilder = _typeBuilder.DefineMethod( $"get_{name}", getterSetterAttributes, typeof( T ), Type.EmptyTypes );
+         ILGenerator getter = getterBuilder.GetILGenerator();
+
+         getter.Emit( OpCodes.Ldarg_0 );
+         getter.Emit( OpCodes.Ldfld, fieldBuilder );
+         getter.Emit( OpCodes.Ret );
+
+         // Build the property "setter"
+
+         var typeArray = ArrayHelper.Create( typeof ( T ) );
+         var setterBuilder = _typeBuilder.DefineMethod( $"set_{name}", getterSetterAttributes, null, typeArray );
+         ILGenerator setter = setterBuilder.GetILGenerator();
+
+         setter.Emit( OpCodes.Ldarg_0 );
+         setter.Emit( OpCodes.Ldarg_1 );
+         setter.Emit( OpCodes.Stfld, fieldBuilder );
+         setter.Emit( OpCodes.Ret );
+
+         // Wire up the getter and setter to the property itself
+
+         propertyBuilder.SetGetMethod( getterBuilder );
+         propertyBuilder.SetSetMethod( setterBuilder );
       }
    }
 }
