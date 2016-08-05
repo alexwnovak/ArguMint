d4756d070bac888c9237c7f12bf8fb3e8e6750c8
Alex Novak <alexwnovak@gmail.com>
2016-08-05 10:03:51 -0400

    +Create factory method to initialize an instance as a builder
    Users will go through this method (instead of a constructor) to obtain
    an instance. This allows the class to do setup and pass any constructor
    parameters it needs--in this case, a TypeBuilder.
    
    It starts by defining the new dynamic assembly, the module, and the new
    type (whose name is generated, since we don't care about the name). It
    hands the TypeBuilder off to a new instance and kicks it back.
    
    Next steps, the caller would call more things on this ClassBuilder to
    add more things to the object we're building--properties, then
    attributes, etc.

--- src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilder.cs
+++ src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilder.cs
@@ -1,6 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
 namespace ArguMint.UnitTests.Dynamic
 {
    public class ClassBuilder
    {
+      private readonly TypeBuilder _typeBuilder;
+
+      private ClassBuilder( TypeBuilder typeBuilder )
+      {
+         _typeBuilder = typeBuilder;
+      }
+
+      public static ClassBuilder Create()
+      {
+         var assemblyName = new AssemblyName( $"ClassBuilderProxyAssembly_{Guid.NewGuid()}" );
+
+         AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly( assemblyName, AssemblyBuilderAccess.Run );
+         ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule( "MainModule" );
+
+         var typeName = $"DynamicClass_{Guid.NewGuid()}";
+
+         TypeBuilder typeBuilder = moduleBuilder.DefineType( typeName,
+            TypeAttributes.Public |
+            TypeAttributes.Class |
+            TypeAttributes.AutoClass |
+            TypeAttributes.AnsiClass |
+            TypeAttributes.BeforeFieldInit |
+            TypeAttributes.AutoLayout,
+            typeof( object ) );
+
+         return new ClassBuilder( typeBuilder );
+      }
    }
 }
