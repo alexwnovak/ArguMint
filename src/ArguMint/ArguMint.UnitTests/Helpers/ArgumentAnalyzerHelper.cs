98376ccbf368ff399ab3aaf67a9e39c8aab13525
Alex Novak <alexwnovak@gmail.com>
2016-08-05 10:32:34 -0400

    +ArgumentAnalyzerHelper static class for testing
    This exists to help facilitate using the ArgumentAnalyzer. That guy's
    Analyze<T> method accepts a generic parameter, which must be known at
    compile time, something that we don't have anymore with these dynamic
    classes.
    
    We'll only use this class in tests to facilitate calling that Analyze
    method WITHOUT a generic parameter, even though we have the Type
    instance that would satisfy it.

---
+++ src/ArguMint/ArguMint.UnitTests/Helpers/ArgumentAnalyzerHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace ArguMint.UnitTests.Helpers
+{
+   public static class ArgumentAnalyzerHelper
+   {
+      public static object Analyze( ArgumentAnalyzer argumentAnalyzer, Type argumentClassType, string[] arguments )
+      {
+         var analyzeMethod = argumentAnalyzer.GetType().GetMethod( "Analyze", BindingFlags.Public | BindingFlags.Instance );
+         var closedAnalyzeMethod = analyzeMethod.MakeGenericMethod( argumentClassType );
+
+         var parameters = new object[]
+         {
+            arguments
+         };
+
+         return closedAnalyzeMethod.Invoke( argumentAnalyzer, parameters );
+      }
+   }
+}
