759975607357ed2aedaab04c69c6d35d3d5927f5
Alex Novak <alexwnovak@gmail.com>
2016-08-05 10:29:13 -0400

    +ObjectExtensions class and Property extension method
    This is designed to work with a fluent assertion, providing the value
    of a property (by property name) using reflection.
    
    For these dynamic argument classes, we can't ever hold a strongly-type
    instance of it in code--we can only get at it through reflection. So as
    the argument analyzer spits them out, we can't "dot" into it to assert
    what its properties are. Instead, we'll use this to make a nicer syntax
    even though it's straight reflection in the end.

---
+++ src/ArguMint/ArguMint.UnitTests/Helpers/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace ArguMint.UnitTests.Helpers
+{
+   public static class ObjExtensions
+   {
+      public static object Property( this object obj, string propertyName )
+      {
+         var propertyInfo = obj.GetType().GetProperty( propertyName, BindingFlags.Public | BindingFlags.Instance );
+         return propertyInfo.GetValue( obj );
+      }
+   }
+}
