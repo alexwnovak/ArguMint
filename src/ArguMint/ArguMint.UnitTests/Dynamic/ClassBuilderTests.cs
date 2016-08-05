2cb27f5d08af3cb0999920ba5cfd50e045402f73
Alex Novak <alexwnovak@gmail.com>
2016-08-05 10:57:31 -0400

    First test of an initial state

--- src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilderTests.cs
+++ src/ArguMint/ArguMint.UnitTests/Dynamic/ClassBuilderTests.cs
@@ -1,6 +1,14 @@
-﻿namespace ArguMint.UnitTests.Dynamic
+﻿using FluentAssertions;
+
+namespace ArguMint.UnitTests.Dynamic
 {
    public class ClassBuilderTests
    {
+      public void Create_DoesNoSetup_TypeIsNull()
+      {
+         var classBuilder = ClassBuilder.Create();
+
+         classBuilder.Type.Should().BeNull();
+      }
    }
 }
