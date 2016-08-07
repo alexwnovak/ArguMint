06425b1c903968e97c4432f37c8c76c6ad40c5bf
Alex Novak <alexwnovak@gmail.com>
2016-08-07 17:05:21 -0400

    Moving integration style tests to correct project

--- src/ArguMint/ArguMint.IntegrationTests/ArgumentAnalyzerTests.cs
+++ src/ArguMint/ArguMint.IntegrationTests/ArgumentAnalyzerTests.cs
@@ -1,6 +1,150 @@
-﻿namespace ArguMint.IntegrationTests
+﻿using FluentAssertions;
+using ArguMint.TestCommon.Dynamic;
+using ArguMint.TestCommon.Helpers;
+
+namespace ArguMint.IntegrationTests
 {
    public class ArgumentAnalyzerTests
    {
+      public void Analyze_ClassHasFirstArgumentAttribute_MapsArgument()
+      {
+         const string propertyName = "FileName";
+         const string fileName = "SomeFileName.txt";
+
+         // Arrange
+
+         var argumentClass = ClassBuilder.Create();
+         argumentClass.AddProperty<string>( propertyName );
+         argumentClass.AddAttribute( propertyName, () => new ArgumentAttribute
+         {
+            Position = ArgumentPosition.First
+         } );
+         argumentClass.Build();
+
+         // Act
+
+         var stringArgs = ArrayHelper.Create( fileName );
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var arguments = ArgumentAnalyzerHelper.Analyze( argumentAnalyzer, argumentClass.Type, stringArgs );
+
+         // Assert
+
+         arguments.Property( propertyName ).Should().Be( fileName );
+      }
+
+      public void Analyze_ClassHasFirstArgumentAttributeButNotTheArgument_DoesNotSet()
+      {
+         const string propertyNameOne = "SourceFileName";
+         const string propertyNameTwo = "DestinationFileName";
+         const string firstArgument = "Source.txt";
+         const string secondArgument = "Destination.txt";
+
+         // Arrange
+
+         var argumentClass = ClassBuilder.Create();
+         argumentClass.AddProperty<string>( propertyNameOne );
+         argumentClass.AddAttribute( propertyNameOne, () => new ArgumentAttribute
+         {
+            Position = ArgumentPosition.First
+         } );
+         argumentClass.AddProperty<string>( propertyNameTwo );
+         argumentClass.AddAttribute( propertyNameTwo, () => new ArgumentAttribute
+         {
+            Position = ArgumentPosition.Second
+         } );
+         argumentClass.Build();
+
+         // Act
+
+         var stringArgs = ArrayHelper.Create( firstArgument, secondArgument );
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var arguments = ArgumentAnalyzerHelper.Analyze( argumentAnalyzer, argumentClass.Type, stringArgs );
+
+         // Assert
+
+         arguments.Property( propertyNameOne ).Should().Be( firstArgument );
+         arguments.Property( propertyNameTwo ).Should().Be( secondArgument );
+      }
+
+      public void Analyze_HasSecondPositionAttributeAndOneArgument_DoesNotSet()
+      {
+         const string propertyName = "SomeArgument";
+
+         // Arrange
+
+         var argumentClass = ClassBuilder.Create();
+         argumentClass.AddProperty<string>( propertyName );
+         argumentClass.AddAttribute( propertyName, () => new ArgumentAttribute
+         {
+            Position = ArgumentPosition.Second
+         } );
+         argumentClass.Build();
+
+         // Act
+
+         var stringArgs = ArrayHelper.Create( "oneargumentbutnottwo" );
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var arguments = ArgumentAnalyzerHelper.Analyze( argumentAnalyzer, argumentClass.Type, stringArgs );
+
+         // Assert
+
+         arguments.Property( propertyName ).Should().BeNull();
+      }
+
+      public void Analyze_HasPrefixPropertyAndOneMatch_SetsValue()
+      {
+         const string propertyName = "FileName";
+         const string fileName = "FileName.txt";
+
+         // Arrange
+
+         var argumentClass = ClassBuilder.Create();
+         argumentClass.AddProperty<string>( propertyName );
+         argumentClass.AddAttribute( propertyName, () => new ArgumentAttribute( "/f:", Spacing.None ) );
+         argumentClass.Build();
+
+         // Act
+
+         var stringArgs = ArrayHelper.Create( $"/f:{fileName}" );
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var arguments = ArgumentAnalyzerHelper.Analyze( argumentAnalyzer, argumentClass.Type, stringArgs );
+
+         // Assert
+
+         arguments.Property( propertyName ).Should().Be( fileName );
+      }
+
+      public void Analyze_HasPrefixPropertyWithSpaceAndOneMatch_SetsValue()
+      {
+         const string propertyName = "FileName";
+         const string fileName = "Video.mp4";
+
+         // Arrange
+
+         var argumentClass = ClassBuilder.Create();
+         argumentClass.AddProperty<string>( propertyName );
+         argumentClass.AddAttribute( propertyName, () => new ArgumentAttribute( "-filename", Spacing.Postfix ) );
+         argumentClass.Build();
+
+         // Act
+
+         var stringArgs = ArrayHelper.Create( "-filename", fileName );
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var arguments = ArgumentAnalyzerHelper.Analyze( argumentAnalyzer, argumentClass.Type, stringArgs );
+
+         // Assert
+
+         arguments.Property( propertyName ).Should().Be( fileName );
+      }
    }
 }
---
+++ src/ArguMint/ArguMint.IntegrationTests/Helpers/ArgumentAnalyzerHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace ArguMint.TestCommon.Helpers
+{
+   public static class ArgumentAnalyzerHelper
+   {
+      public static object Analyze( ArgumentAnalyzer argumentAnalyzer, Type argumentClassType, string[] arguments )
+      {
+         var analyzeMethod = argumentAnalyzer.GetType().GetMethod( "Analyze", BindingFlags.Public | BindingFlags.Instance );
+         var closedAnalyzeMethod = analyzeMethod.MakeGenericMethod( argumentClassType );
+
+         var parameters = new object[]
+         {
+            arguments
+         };
+
+         try
+         {
+            return closedAnalyzeMethod.Invoke( argumentAnalyzer, parameters );
+         }
+         catch ( TargetInvocationException ex ) when ( ex.InnerException != null )
+         {
+            throw ex.InnerException;
+         }
+      }
+   }
+}
