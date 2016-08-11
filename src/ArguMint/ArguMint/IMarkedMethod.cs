65e8adf93a5e15a29087a7793962ed534b53566b
Alex Novak <alexwnovak@gmail.com>
2016-08-11 12:39:52 -0400

    +MarkedMethod implementation

--- src/ArguMint/ArguMint/IMarkedMethod.cs
+++ src/ArguMint/ArguMint/IMarkedMethod.cs
@@ -4,5 +4,16 @@ namespace ArguMint
 {
    internal interface IMarkedMethod<out T> where T : Attribute
    {
+      T Attribute
+      {
+         get;
+      }
+
+      string Name
+      {
+         get;
+      }
+
+      void Invoke( object instance );
    }
 }
--- src/ArguMint/ArguMint/MarkedMethod.cs
+++ src/ArguMint/ArguMint/MarkedMethod.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.Reflection;
 
 namespace ArguMint
 {
    internal class MarkedMethod<T> : IMarkedMethod<T> where T : Attribute
    {
+      public T Attribute
+      {
+         get;
+      }
+
+      private readonly MethodInfo _methodInfo;
+      public string Name => _methodInfo.Name;
+
+      public MarkedMethod( MethodInfo methodInfo, T attribute )
+      {
+         Attribute = attribute;
+         _methodInfo = methodInfo;
+      }
+
+      public void Invoke( object instance )
+      {
+         _methodInfo.Invoke( instance, new object[0] );
+      }
    }
 }
