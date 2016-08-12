95564f0b971560e9be882fb93f1636be6da2a79b
Alex Novak <alexwnovak@gmail.com>
2016-08-11 23:06:04 -0400

    Fleshing out HandlerDispatcher
    Simple exchange--gets the method from the TypeInspector interface and
    invoke if the conditions are right.

--- src/ArguMint/ArguMint/HandlerDispatcher.cs
+++ src/ArguMint/ArguMint/HandlerDispatcher.cs
@@ -1,6 +1,22 @@
 ﻿namespace ArguMint
 {
-   internal class HandlerDispatcher
+   internal class HandlerDispatcher : IHandlerDispatcher
    {
+      private readonly ITypeInspector _typeInspector;
+
+      public HandlerDispatcher( ITypeInspector typeInspector )
+      {
+         _typeInspector = typeInspector;
+      }
+
+      public void DispatchArgumentsOmitted( object argumentClass )
+      {
+         var markedMethods = _typeInspector.GetMarkedMethods<ArgumentsOmittedHandlerAttribute>( argumentClass.GetType() );
+
+         if ( markedMethods?.Length == 1 )
+         {
+            markedMethods[0].Invoke( argumentClass );
+         }
+      }
    }
 }
--- src/ArguMint/ArguMint/IHandlerDispatcher.cs
+++ src/ArguMint/ArguMint/IHandlerDispatcher.cs
@@ -2,5 +2,6 @@
 {
    internal interface IHandlerDispatcher
    {
+      void DispatchArgumentsOmitted( object argumentClass );
    }
 }
