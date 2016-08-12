3d45929a69145726e77746f786088505a6afbf3f
Alex Novak <alexwnovak@gmail.com>
2016-08-12 18:35:11 -0400

    +Test to show that the marked method will be called

--- src/ArguMint/ArguMint.UnitTests/HandlerDispatcherTests.cs
+++ src/ArguMint/ArguMint.UnitTests/HandlerDispatcherTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FluentAssertions;
 using Moq;
+using ArguMint.TestCommon.Helpers;
 
 namespace ArguMint.UnitTests
 {
@@ -19,6 +20,29 @@ namespace ArguMint.UnitTests
          Action dispatchArgumentsOmitted = () => handlerDispatcher.DispatchArgumentsOmitted( null );
 
          dispatchArgumentsOmitted.ShouldThrow<ArgumentException>();
+      }
+
+      public void DispatchArgumentsOmitted_FindsOneMarkedMethod_CallsTheMarkedMethod()
+      {
+         // Arrange
+
+         var markedMethodMock = new Mock<IMarkedMethod<ArgumentsOmittedHandlerAttribute>>();
+         var markedMethods = ArrayHelper.Create( markedMethodMock.Object );
+
+         var typeInspectorMock = new Mock<ITypeInspector>();
+         typeInspectorMock.Setup( ti => ti.GetMarkedMethods<ArgumentsOmittedHandlerAttribute>( It.IsAny<Type>() ) ).Returns( markedMethods );
+
+         // Act
+
+         object argumentClassDoesNotMatter = 12345;
+
+         var handlerDispatcher = new HandlerDispatcher( typeInspectorMock.Object );
+
+         handlerDispatcher.DispatchArgumentsOmitted( argumentClassDoesNotMatter );
+
+         // Assert
+
+         markedMethodMock.Verify( mm => mm.Invoke( argumentClassDoesNotMatter ), Times.Once() );
       }
    }
 }
