db5c7c5b7876eb0e0ba56159aa1cd98d815dcf77
Alex Novak <alexwnovak@gmail.com>
2016-08-12 20:50:35 -0400

    +ArgumentConfigurationException
    This will be thrown when the argument class is configured improperly.
    It's designed to help out the developer when things are wired wrong.
    It shouldn't be something that gets thrown for real at runtime.

---
+++ src/ArguMint/ArguMint/ArgumentConfigurationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ArguMint
+{
+   [Serializable]
+   public class ArgumentConfigurationException : Exception
+   {
+      public ArgumentConfigurationException()
+      {
+      }
+
+      public ArgumentConfigurationException( string message )
+         : base( message )
+      {
+      }
+
+      public ArgumentConfigurationException( string message, Exception inner )
+         : base( message, inner )
+      {
+      }
+
+      protected ArgumentConfigurationException( SerializationInfo info, StreamingContext context )
+         : base( info, context )
+      {
+      }
+   }
+}
