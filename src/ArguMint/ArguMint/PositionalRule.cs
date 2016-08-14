e6652c74baafacfe25a1fe0301021695315095ce
Alex Novak <alexwnovak@gmail.com>
2016-08-14 09:31:34 -0400

    Putting the rule logic into a few rules
    This used to be the smarts that were in ArgumentAnalyzer. Now we're
    stashing them into two classes that can be applied separately, since
    the logic was othrogonal in the first place.
    
    Not sure if this will work for all our cases, but it's a start.

---
+++ src/ArguMint/ArguMint/PositionalRule.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace ArguMint
+{
+   internal class PositionalRule : IArgumentRule
+   {
+      public void Match( object argumentClass, IMarkedProperty<ArgumentAttribute> property, string[] arguments )
+      {
+         if ( property.Attribute.Position != ArgumentPosition.Any )
+         {
+            int index = property.Attribute.Position.ToIndex();
+
+            if ( arguments.Length >= index + 1 )
+            {
+               var typeConverter = TypeDescriptor.GetConverter( property.PropertyType );
+
+               object convertedValue = typeConverter.ConvertFromString( arguments[index] );
+
+               property.SetPropertyValue( argumentClass, convertedValue );
+            }
+         }
+      }
+   }
+}
---
+++ src/ArguMint/ArguMint/PrefixRule.cs
@@ -0,0 +1,34 @@
+﻿namespace ArguMint
+{
+   internal class PrefixRule : IArgumentRule
+   {
+      public void Match( object argumentClass, IMarkedProperty<ArgumentAttribute> property, string[] arguments )
+      {
+         if ( !string.IsNullOrEmpty( property.Attribute.Argument ) )
+         {
+            if ( property.Attribute.Spacing == Spacing.None )
+            {
+               foreach ( string argument in arguments )
+               {
+                  if ( argument.StartsWith( property.Attribute.Argument ) )
+                  {
+                     string value = argument.Replace( property.Attribute.Argument, string.Empty );
+                     property.SetPropertyValue( argumentClass, value );
+                  }
+               }
+            }
+            else if ( property.Attribute.Spacing == Spacing.Postfix )
+            {
+               for ( int index = 0; index < arguments.Length; index++ )
+               {
+                  if ( arguments[index] == property.Attribute.Argument )
+                  {
+                     string value = arguments[index + 1];
+                     property.SetPropertyValue( argumentClass, value );
+                  }
+               }
+            }
+         }
+      }
+   }
+}
