c738954913f0ed09772f566752e4a6974887f40d
Alex Novak <alexwnovak@gmail.com>
2016-08-13 21:55:43 -0400

    RuleMatcher now throws if no marked properties are found
    This used to be ArgumentAnalyzer's responsibility.

--- src/ArguMint/ArguMint.UnitTests/RuleMatcherTests.cs
+++ src/ArguMint/ArguMint.UnitTests/RuleMatcherTests.cs
@@ -1,6 +1,30 @@
-﻿namespace ArguMint.UnitTests
+﻿using System;
+using FluentAssertions;
+using Moq;
+
+namespace ArguMint.UnitTests
 {
    public class RuleMatcherTests
    {
+      public void Match_NoMarkedPropertiesFound_ThrowsArgumentConfigurationException()
+      {
+         IMarkedProperty<ArgumentAttribute>[] markedProperties = new IMarkedProperty<ArgumentAttribute>[0];
+
+         // Arrange
+
+         var typeInspectorMock = new Mock<ITypeInspector>();
+         typeInspectorMock.Setup( ti => ti.GetMarkedProperties<ArgumentAttribute>( It.IsAny<Type>() ) ).Returns( markedProperties );
+
+         // Act
+
+         object argumentClass = "DoesNotMatterWhatThisIs";
+
+         var ruleMatcher = new RuleMatcher( typeInspectorMock.Object );
+         Action match = () => ruleMatcher.Match( argumentClass, null );
+
+         // Assert
+
+         match.ShouldThrow<ArgumentConfigurationException>();
+      }
    }
 }
--- src/ArguMint/ArguMint/RuleMatcher.cs
+++ src/ArguMint/ArguMint/RuleMatcher.cs
@@ -4,9 +4,21 @@ namespace ArguMint
 {
    internal class RuleMatcher : IRuleMatcher
    {
+      private readonly ITypeInspector _typeInspector;
+
+      public RuleMatcher( ITypeInspector typeInspector )
+      {
+         _typeInspector = typeInspector;
+      }
+
       public void Match( object argumentClass, string[] arguments )
       {
-         throw new NotImplementedException();
+         var markedProperties = _typeInspector.GetMarkedProperties<ArgumentAttribute>( argumentClass.GetType() );
+
+         if ( markedProperties.Length == 0 )
+         {
+            throw new ArgumentConfigurationException( "Argument class must have argument properties denoted by ArgumentAttributes" );
+         }
       }
    }
 }
