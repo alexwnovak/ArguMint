fb84c5e40a4bb2091678bef1c075195194b45e84
Alex Novak <alexwnovak@gmail.com>
2016-08-13 21:42:20 -0400

    ArgumentAnalyzer now hands it off to the RuleMatcher
    Now has one of these and defers to it. This removes basically all the
    actual work from this class, who defers to the set of classes that are
    truly responsible for doing the core value.

--- src/ArguMint/ArguMint/ArgumentAnalyzer.cs
+++ src/ArguMint/ArguMint/ArgumentAnalyzer.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace ArguMint
 {
    public class ArgumentAnalyzer
    {
-      private readonly ITypeInspector _typeInspector;
       private readonly IHandlerDispatcher _handlerDispatcher;
+      private readonly IRuleMatcher _ruleMatcher;
 
       public ArgumentAnalyzer()
       {
-         _typeInspector = new TypeInspector();
-         _handlerDispatcher = new HandlerDispatcher( _typeInspector );
+         _handlerDispatcher = new HandlerDispatcher( new TypeInspector() );
+         _ruleMatcher = new RuleMatcher( new TypeInspector() );
       }
 
-      internal ArgumentAnalyzer( ITypeInspector typeInspector, IHandlerDispatcher handlerDispatcher )
+      internal ArgumentAnalyzer( IHandlerDispatcher handlerDispatcher, IRuleMatcher ruleMatcher )
       {
-         _typeInspector = typeInspector;
          _handlerDispatcher = handlerDispatcher;
+         _ruleMatcher = ruleMatcher;
       }
 
       public T Analyze<T>( string[] arguments ) where T : class, new()
@@ -36,66 +35,9 @@ namespace ArguMint
             return argumentClass;
          }
 
-         var markedProperties = GetMarkedProperties<T>();
-
-         foreach ( var markedProperty in markedProperties )
-         {
-            if ( markedProperty.Attribute.Position != ArgumentPosition.Any )
-            {
-               int index = markedProperty.Attribute.Position.ToIndex();
-
-               if ( arguments.Length >= index + 1 )
-               {
-                  var typeConverter = TypeDescriptor.GetConverter( markedProperty.PropertyType );
-
-                  object convertedValue = typeConverter.ConvertFromString( arguments[index] );
-
-                  markedProperty.SetPropertyValue( argumentClass, convertedValue );
-               }
-            }
-            else
-            {
-               if ( !string.IsNullOrEmpty( markedProperty.Attribute.Argument ) )
-               {
-                  if ( markedProperty.Attribute.Spacing == Spacing.None )
-                  {
-                     foreach ( string argument in arguments )
-                     {
-                        if ( argument.StartsWith( markedProperty.Attribute.Argument ) )
-                        {
-                           string value = argument.Replace( markedProperty.Attribute.Argument, string.Empty );
-                           markedProperty.SetPropertyValue( argumentClass, value );
-                        }
-                     }
-                  }
-                  else if ( markedProperty.Attribute.Spacing == Spacing.Postfix )
-                  {
-                     for ( int index = 0; index < arguments.Length; index++ )
-                     {
-                        if ( arguments[index] == markedProperty.Attribute.Argument )
-                        {
-                           string value = arguments[index + 1];
-                           markedProperty.SetPropertyValue( argumentClass, value );
-                        }
-                     }
-                  }
-               }
-            }
-         }
+         _ruleMatcher.Match( argumentClass, arguments );
 
          return argumentClass;
-      }
-
-      private IMarkedProperty<ArgumentAttribute>[] GetMarkedProperties<T>()
-      {
-         var markedProperties = _typeInspector.GetMarkedProperties<ArgumentAttribute>( typeof( T ) );
-
-         if ( markedProperties.Length == 0 )
-         {
-            throw new MissingAttributesException( "No Argument attributes were found on class: " + typeof( T ).Name );
-         }
-
-         return markedProperties;
       }
    }
 }
