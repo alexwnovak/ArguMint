c726a8e9e220fc368f09fa56230d42b9a65e0eb6
Alex Novak <alexwnovak@gmail.com>
2016-08-14 18:28:35 -0400

    Can now convert strings into strongly-typed objects

--- src/ArguMint/ArguMint.UnitTests/ValueConverterTests.cs
+++ src/ArguMint/ArguMint.UnitTests/ValueConverterTests.cs
@@ -1,6 +1,27 @@
-﻿namespace ArguMint.UnitTests
+﻿using FluentAssertions;
+
+namespace ArguMint.UnitTests
 {
    public class ValueConverterTests
    {
+      public void Convert_SourceObjectIsString_ReturnsStringWithSameValue()
+      {
+         string sourceObject = "";
+
+         var convertedObject = ValueConverter.Convert( sourceObject, sourceObject.GetType() );
+
+         convertedObject.Should().BeOfType<string>();
+         convertedObject.Should().Be( "" );
+      }
+
+      public void Convert_SourceObjectIsInteger_ReturnsIntegerWithSameValue()
+      {
+         int sourceObject = 123;
+
+         var convertedObject = ValueConverter.Convert( sourceObject.ToString(), sourceObject.GetType() );
+
+         convertedObject.Should().BeOfType<int>();
+         convertedObject.Should().Be( sourceObject );
+      }
    }
 }
--- src/ArguMint/ArguMint/ValueConverter.cs
+++ src/ArguMint/ArguMint/ValueConverter.cs
@@ -1,7 +1,15 @@
-﻿namespace ArguMint
+﻿using System;
+using System.ComponentModel;
+
+namespace ArguMint
 {
    internal static class ValueConverter
    {
+      public static object Convert( string stringValue, Type destinationType )
+      {
+         var typeConverter = TypeDescriptor.GetConverter( destinationType );
 
+         return typeConverter.ConvertFromString( stringValue );
+      }
    }
 }
