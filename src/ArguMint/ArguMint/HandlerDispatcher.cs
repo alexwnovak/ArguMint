0363034f69139deda48b2f264db57f2f1bc241a0
Alex Novak <alexwnovak@gmail.com>
2016-08-15 12:23:33 -0400

    Extracting the bulk of the discovery, validation and call logic
    This boils down each "dispatch" method into a pass-through, passing
    along the kind of attribute it cares about finding (for the particular
    handler). Moved the rest of it into the generic guy.
    
    Churned the null-check-and-throw stuff back into this method, since it
    felt better to have it all in one, instead of a one-off method for
    validation.

--- src/ArguMint/ArguMint/HandlerDispatcher.cs
+++ src/ArguMint/ArguMint/HandlerDispatcher.cs
@@ -11,55 +11,34 @@ namespace ArguMint
          _typeInspector = typeInspector;
       }
 
-      private static void ValidateArgumentClass( object argumentClass )
+      public void DispatchForAttribute<T>( object argumentClass ) where T : Attribute
       {
          if ( argumentClass == null )
          {
             throw new ArgumentException( "Argument class must not be null" );
          }
+
+         var markedMethods = _typeInspector.GetMarkedMethods<T>( argumentClass.GetType() );
+
+         if ( markedMethods != null )
+         {
+            int count = markedMethods.Length;
+
+            if ( count == 1 )
+            {
+               markedMethods[0].Invoke( argumentClass );
+            }
+            else if ( count > 1 )
+            {
+               throw new ArgumentConfigurationException( $"Argument class can only have one {typeof( T ).Name} but found {count}" );
+            }
+         }
       }
 
       public void DispatchArgumentsOmitted( object argumentClass )
-      {
-         ValidateArgumentClass( argumentClass );
-
-         var markedMethods = _typeInspector.GetMarkedMethods<ArgumentsOmittedHandlerAttribute>( argumentClass.GetType() );
-
-         if ( markedMethods != null )
-         {
-            int count = markedMethods.Length;
-
-            if ( count == 1 )
-            {
-               markedMethods[0].Invoke( argumentClass );
-            }
-            else if ( count > 1 )
-            {
-               throw new ArgumentConfigurationException( $"Argument class can only have one ArgumentsOmittedHandler but found {count}" );
-            }
-         }
-      }
+         => DispatchForAttribute<ArgumentsOmittedHandlerAttribute>( argumentClass );
 
       public void DispatchArgumentError( object argumentClass )
-      {
-         ValidateArgumentClass( argumentClass );
-
-         var markedMethods = _typeInspector.GetMarkedMethods<ArgumentErrorHandlerAttribute>( argumentClass.GetType() );
-
-         if ( markedMethods != null )
-         {
-            int count = markedMethods.Length;
-
-            if ( count == 1 )
-            {
-               markedMethods[0].Invoke( argumentClass );
-            }
-            else if ( count > 1 )
-            {
-               throw new ArgumentConfigurationException( $"Argument class can only have one ArgumentErrorHandler but found {count}" );
-            }
-
-         }
-      }
+         => DispatchForAttribute<ArgumentErrorHandlerAttribute>( argumentClass );
    }
 }
