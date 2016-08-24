3c576d12b1f2adfdfda0915eced5b62449b45daa
Alex Novak <alexwnovak@gmail.com>
2016-08-23 22:50:25 -0400

    +First scenario test to match valid arguments

--- src/ArguMint/ArguMint.IntegrationTests/Scenarios/FileCopy/FileCopyScenarioTests.cs
+++ src/ArguMint/ArguMint.IntegrationTests/Scenarios/FileCopy/FileCopyScenarioTests.cs
@@ -1,6 +1,26 @@
+﻿using ArguMint.TestCommon.Helpers;
+using FluentAssertions;
+
 namespace ArguMint.IntegrationTests.Scenarios.FileCopy
 {
    public class FileCopyScenarioTests
    {
+      public void FileCopyScenario_ContainsTheTwoArguments_MatchesArguments()
+      {
+         const string sourceFile = @"C:\Temp\Source.txt";
+         const string destinationFile = @"C:\Temp\Destination.txt";
+         var stringArgs = ArrayHelper.Create( sourceFile, destinationFile );
+
+         // Act
+
+         var argumentAnalyzer = new ArgumentAnalyzer();
+
+         var argumentClass = argumentAnalyzer.Analyze<FileCopyArguments>( stringArgs );
+
+         // Asserts
+
+         argumentClass.SourceFile.Should().Be( sourceFile );
+         argumentClass.DestinationFile.Should().Be( destinationFile );
+      }
    }
 }
