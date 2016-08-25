4f9abe458138122a9fb43721c1d765f67aae277b
Alex Novak <alexwnovak@gmail.com>
2016-08-24 22:44:17 -0400

    +Property and constructor setter for error type
    Not the property name yet. We'll back fill the existing functionality
    with this new type before adding new stuff.
    
    Also made this constructor public, since it seems possible that the
    user would want to make these themselves, possibly in their own unit
    test scenarios when using this library.

--- src/ArguMint/ArguMint/ArgumentError.cs
+++ src/ArguMint/ArguMint/ArgumentError.cs
@@ -2,5 +2,14 @@
 {
    public class ArgumentError
    {
+      public ArgumentErrorType ErrorType
+      {
+         get;
+      }
+
+      public ArgumentError( ArgumentErrorType errorType )
+      {
+         ErrorType = errorType;
+      }
    }
 }
