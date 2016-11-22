51edd6e8cc6de98307adef895eb3cce419fe263a
Alex Novak <alexwnovak@gmail.com>
2016-11-21 21:39:53 -0500

    +ArgumentToken internal class for encapsulating a single argument
    This wraps each entry in the Args array, giving a layer of indirection. So far it has a bool to indicate whether it's been matched, so we can iterate over the same arguments many times without consuming them multiple times.

---
+++ src/ArguMint/ArguMint/ArgumentToken.cs
@@ -0,0 +1,21 @@
+﻿namespace ArguMint
+{
+   internal class ArgumentToken
+   {
+      public string Token
+      {
+         get;
+      }
+
+      public bool IsMatched
+      {
+         get;
+         set;
+      }
+
+      public ArgumentToken( string token )
+      {
+         Token = token;
+      }
+   }
+}
