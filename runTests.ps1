00e5a7e1ce21ba2910c5470d29e87d157954ac98
Alex Novak <alexwnovak@gmail.com>
2016-11-22 22:22:44 -0500

    Deleting runTests script
    Uses xUnit runner locally; uses the xUnit runner in Cake.

--- runTests.ps1
+++
@@ -1,2 +0,0 @@
-$testAssemblies = gci *Test*.dll -r | ? { $_.FullName.ToLower().Contains( "bin" ) } | Sort-Object Name -Unique
-.\src\ArguMint\packages\Fixie.1.0.2\lib\net45\Fixie.Console.exe $testAssemblies
