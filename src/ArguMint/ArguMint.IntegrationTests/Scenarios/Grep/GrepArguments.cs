bf2fc356b18dbe1cfc81e4d20a5951787bd5d5fc
Alex Novak <alexwnovak@gmail.com>
2016-11-27 16:30:30 -0500

    Now has an ignore case flag and a file name property to match

--- src/ArguMint/ArguMint.IntegrationTests/Scenarios/Grep/GrepArguments.cs
+++ src/ArguMint/ArguMint.IntegrationTests/Scenarios/Grep/GrepArguments.cs
@@ -2,5 +2,18 @@
 {
    public class GrepArguments
    {
+      [Argument( "--ignore-case" )]
+      public bool IgnoreCase
+      {
+         get;
+         set;
+      }
+
+      [Argument( Position = ArgumentPosition.Last )]
+      public string FileName
+      {
+         get;
+         set;
+      }
    }
 }
--- src/ArguMint/ArguMint.IntegrationTests/Scenarios/Grep/GrepScenarioTests.cs
+++ src/ArguMint/ArguMint.IntegrationTests/Scenarios/Grep/GrepScenarioTests.cs
@@ -1,6 +1,42 @@
-﻿namespace ArguMint.IntegrationTests.Scenarios.Grep
+﻿using FluentAssertions;
+using Xunit;
+using ArguMint.TestCommon.Helpers;
+
+namespace ArguMint.IntegrationTests.Scenarios.Grep
 {
    public class GrepScenarioTests
    {
+      [Fact]
+      public void GrepScenario_HasFileName_MatchesFileName()
+      {
+         const string fileName = @"C:\Temp\Document.txt";
+         var stringArgs = ArrayHelper.Create( fileName );
+
+         // Act
+
+         var argumentClass = ArgumentAnalyzer.Analyze<GrepArguments>( stringArgs );
+
+         // Assert
+
+         argumentClass.FileName.Should().Be( fileName );
+         argumentClass.IgnoreCase.Should().BeFalse();
+      }
+
+      [Fact]
+      public void GrepScenario_HasFileNameAndIgnoreCaseFlag_MatchesInOrder()
+      {
+         const string ignoreCase = "--ignore-case";
+         const string fileName = @"C:\Temp\Document.txt";
+         var stringArgs = ArrayHelper.Create( ignoreCase, fileName );
+
+         // Act
+
+         var argumentClass = ArgumentAnalyzer.Analyze<GrepArguments>( stringArgs );
+
+         // Assert
+
+         argumentClass.FileName.Should().Be( fileName );
+         argumentClass.IgnoreCase.Should().BeTrue();
+      }
    }
 }
